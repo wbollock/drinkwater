87958a39c41996351882a608962a36e4cb7d2bf8
Will Bollock <wbollock@gmail.com>
2019-06-17 14:44:49 -0400

    working

---
+++ drinkwater.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Made by Will Bollock to remind me to drink water
+while true; do
+notify-send --icon="$HOME/projects/bash/drinkWater/drinkwater.gif" "Reminder: Stay hydrated"
+sleep 3600
+done
+
